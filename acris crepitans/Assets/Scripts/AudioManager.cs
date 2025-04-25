5fc8315c8a75731560c8f8344afc600307dc71bb
jollyjumper-exe <mortimer.van.egmond@proton.me>
2025-04-25 12:14:57 +0200

    feat: more music

--- acris crepitans/Assets/Scripts/AudioManager.cs
+++ acris crepitans/Assets/Scripts/AudioManager.cs
@@ -1,97 +1,77 @@
 using UnityEngine;
+using System.Collections.Generic;
 
 public class AudioManager : MonoBehaviour
 {
-        public static AudioManager Instance;
+    public static AudioManager Instance;
 
-        [Header("Music")]
-        [SerializeField] private AudioSource musicSource;
-        public AudioSource MusicSource => musicSource;
-        private AudioLowPassFilter musicLowPassFilter;
+    [Header("Music")]
+    [SerializeField] private AudioSource musicSource;
+    public AudioSource MusicSource => musicSource;
+    private AudioLowPassFilter musicLowPassFilter;
 
-        [Header("Sound Effects")]
-        [SerializeField] private AudioSource playerSource;
-        [SerializeField] private AudioClip hoverClip;
-        [SerializeField] private AudioClip jumpClip;
-        [SerializeField] private AudioClip coinClip;
-        [SerializeField] private AudioClip coinBonusClip;
-        [SerializeField] private AudioClip damageClip;
-        [SerializeField] private AudioClip deadClip;
+    [SerializeField] private List<AudioClip> musicClips;
 
+    [Header("Sound Effects")]
+    [SerializeField] private AudioSource playerSource;
+    [SerializeField] private AudioClip hoverClip;
+    [SerializeField] private AudioClip jumpClip;
+    [SerializeField] private AudioClip coinClip;
+    [SerializeField] private AudioClip coinBonusClip;
+    [SerializeField] private AudioClip damageClip;
+    [SerializeField] private AudioClip deadClip;
 
-        private void Awake()
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
         {
-            if (Instance != null && Instance != this)
-            {
-                Destroy(gameObject);
-                return;
-            }
-
-            Instance = this;
-
-            musicLowPassFilter = musicSource.GetComponent<AudioLowPassFilter>();
-        }
-        
-        public void PlayJump()
-        {
-            DefatigueAndPlayClip(playerSource, jumpClip);
+            Destroy(gameObject);
+            return;
         }
 
-        public void PlayHover()
-        {
-            DefatigueAndPlayClip(playerSource, hoverClip);
-        }
+        Instance = this;
+        musicLowPassFilter = musicSource.GetComponent<AudioLowPassFilter>();
+    }
 
-        public void PlayCoin()
+    public void StartMusic()
+    {
+        if (musicSource != null && !musicSource.isPlaying && musicClips.Count > 0)
         {
-            DefatigueAndPlayClip(playerSource, coinClip);
+            int randomIndex = Random.Range(0, musicClips.Count);
+            musicSource.clip = musicClips[randomIndex];
+            musicSource.Play();
         }
+    }
 
-        public void PlayCoinBonus()
+    public void MuffleMusic(bool muffle)
+    {
+        if (musicLowPassFilter == null) return;
+
+        musicLowPassFilter.cutoffFrequency = muffle ? 500f : 22000f;
+    }
+
+    private void DefatigueAndPlayClip(AudioSource source, AudioClip clip)
+    {
+        if (source != null && clip != null)
         {
-            DefatigueAndPlayClip(playerSource, coinBonusClip);
+            source.pitch = Random.Range(0.95f, 1.05f);
+            source.volume = Random.Range(0.9f, 1.0f);
+            source.PlayOneShot(clip);
         }
+    }
 
-        public void PlayDamage()
+    private void OnDestroy()
+    {
+        if (musicSource != null && musicSource.isPlaying)
         {
-            DefatigueAndPlayClip(playerSource, damageClip);
+            musicSource.Stop();
         }
+    }
 
-        public void PlayDead()
-        {
-            DefatigueAndPlayClip(playerSource, deadClip);
-        }
-
-        private void DefatigueAndPlayClip(AudioSource source, AudioClip clip)
-        {
-            if (source != null && clip != null)
-            {
-                source.pitch = Random.Range(0.95f, 1.05f);
-                source.volume = Random.Range(0.9f, 1.0f);
-                source.PlayOneShot(clip);
-            }
-        }
-
-        public void StartMusic()
-        {
-            if (musicSource != null && !musicSource.isPlaying)
-            {
-                musicSource.Play();
-            }
-        }
-
-        public void MuffleMusic(bool muffle)
-        {
-            if (musicLowPassFilter == null) return;
-
-            musicLowPassFilter.cutoffFrequency = muffle ? 500f : 22000f; 
-        }
-
-        private void OnDestroy()
-        {
-            if (musicSource != null && musicSource.isPlaying)
-            {
-                musicSource.Stop();
-            }
-        }
+    public void PlayJump() => DefatigueAndPlayClip(playerSource, jumpClip);
+    public void PlayHover() => DefatigueAndPlayClip(playerSource, hoverClip);
+    public void PlayCoin() => DefatigueAndPlayClip(playerSource, coinClip);
+    public void PlayCoinBonus() => DefatigueAndPlayClip(playerSource, coinBonusClip);
+    public void PlayDamage() => DefatigueAndPlayClip(playerSource, damageClip);
+    public void PlayDead() => DefatigueAndPlayClip(playerSource, deadClip);
 }
